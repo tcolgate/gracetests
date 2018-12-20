83190aa98c2ac2626be225ac4b05de08c3a07bf8
Tristan Colgate <tristan@qubit.com>
2018-12-20 12:56:48 +0000

    Python example

---
+++ python/graceful/main.py
@@ -0,0 +1,65 @@
+import asyncio
+import datetime
+import logging
+import os
+
+from sanic import Sanic
+from sanic.exceptions import SanicException
+from sanic.response import json
+
+STARTED = datetime.datetime.utcnow().isoformat()
+DIR = os.path.dirname(os.path.realpath(__file__))
+
+app = Sanic()
+app.config.REQUEST_MAX_SIZE = 100000  # 100 KB
+app.config.REQUEST_TIMEOUT = 5  # 5 sec
+app.config.RESPONSE_TIMEOUT = 15  # 15 sec
+app.config.KEEP_ALIVE = False  # Drop connection after resp
+
+
+@app.exception(SanicException)
+def json_error(request, exception):
+    e = exception
+    if request:
+        logging.error(f'Error {e.status_code}, {request.url}: {str(e)}')
+
+    return json({'status': e.status_code, 'message': str(e)}, e.status_code)
+
+
+@app.route('/')
+async def root(req):
+    return json({
+        'hello': 'world'
+    })
+
+
+SHUTTING_DOWN = False    
+@app.listener('before_server_stop')
+async def notify_server_stopping(app, loop):
+    if not os.environ.get('UNGRACEFUL'):
+        print('Sleeping before shutting down!')
+        SHUTTING_DOWN = True    
+        await asyncio.sleep(15)
+        print('Server shutting down!')
+    else:
+        print('Quit immediately')
+
+@app.route('/status')
+async def health_check(req):
+    status = 200
+    if SHUTTING_DOWN:
+        status = 500
+    return json({
+        'service': 'graceful-py',
+        'startedAt': STARTED,
+        'status': 500
+    })
+
+
+def main():
+    app.static('/favicon.ico', DIR + '/favicon.ico')
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
+
+if __name__ == '__main__':
+    main()
---
+++ python/setup.py
@@ -0,0 +1,28 @@
+import os
+
+from setuptools import find_packages, setup
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+
+setup(
+    name='graceful',
+    version='1.2.0',
+    description='test of graceful shutdown',
+    url='https://github.com/qubitdigital/graceful/python',
+    author='Infra',
+    author_email='infra@qubit.com',
+    license='All rights reserved.',
+    packages=find_packages(),
+    install_requires=[
+        'sanic==0.7.0',
+        'ujson==1.35',
+        'python-dotenv==0.8.2',
+        'cchardet==2.1.1',
+    ],
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'graceful=graceful.server:main',
+        ]
+    }
+)
